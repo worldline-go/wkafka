12b031a2e1fd2295891c3b0a4c938891ee13334d
Eray Ates <eray.ates@worldline.com>
2025-01-15 23:41:50 +0100

    fix: lock control
    Signed-off-by: Eray Ates <eray.ates@worldline.com>

--- hook.go
+++ hook.go
@@ -2,17 +2,23 @@ package wkafka
 
 import (
 	"context"
+	"sync"
 
 	"github.com/twmb/franz-go/pkg/kgo"
 )
 
 type hooker struct {
 	ctx context.Context //nolint:containedctx // replace in consume
+
+	m sync.RWMutex
 }
 
 var _ kgo.HookFetchRecordBuffered = new(hooker)
 
 func (h *hooker) OnFetchRecordBuffered(r *kgo.Record) {
+	h.m.RLock()
+	defer h.m.RUnlock()
+
 	r.Context = h.ctx
 }
 
@@ -20,6 +26,9 @@ func (h *hooker) setCtx(ctx context.Context) {
 	if h == nil {
 		return
 	}
+
+	h.m.Lock()
+	defer h.m.Unlock()
 
 	h.ctx = ctx
 }
