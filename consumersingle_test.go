c642b7138b1b7e2dfb12f74771f52e6f8f260124
Fabian Dluski <fabiand9696@gmail.com>
2026-03-13 11:57:13 +0100

    fix: stop consuming when parent context is cancelled (#5)
    Co-authored-by: fdluski <fabian.dluski.external@worldline.com>

--- consumersingle_test.go
+++ consumersingle_test.go
@@ -3,8 +3,10 @@ package wkafka
 import (
 	"context"
 	"errors"
+	"fmt"
 	"slices"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 	"github.com/twmb/franz-go/pkg/kgo"
@@ -162,4 +164,80 @@ func TestConsumerSingle_iterationConcurrent(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestConsumerSingle_iterationRecords(t *testing.T) {
+	t.Run("consumer stops when parent context is cancelled", func(t *testing.T) {
+		processed := make([]any, 0, 20)
+		cs := consumerSingle[any]{
+			customer: &customer[any]{
+				Cfg: &ConsumerConfig{
+					Concurrent: ConcurrentConfig{
+						Process: 20,
+					},
+					Skip:                           make(map[string]map[int32]OffsetConfig),
+					RecoverAfterProcessingError:    true,
+					ProcessPartitionsIndependently: true,
+				},
+				Skip: func(cfg *ConsumerConfig, r *kgo.Record) bool {
+					return false
+				},
+				Decode: func(raw []byte, r *kgo.Record) (any, error) {
+					return raw, nil
+				},
+				Logger: LogNoop{},
+			},
+			Group: newGroupMix(100, 20),
+			PartitionHandler: &partitionHandler{
+				logger: LogNoop{},
+			},
+			Process: func(ctx context.Context, msg any) error {
+				processed = append(processed, msg)
+				time.Sleep(time.Second)
+
+				return nil
+			},
+		}
+
+		fetch := kgo.Fetches{
+			{
+				Topics: []kgo.FetchTopic{
+					{
+						Topic: "test",
+						Partitions: []kgo.FetchPartition{
+							{
+								Partition: 0,
+								Records:   make([]*kgo.Record, 30),
+							},
+						},
+					},
+				},
+			},
+		}
+
+		for i := range 30 {
+			fetch[0].Topics[0].Partitions[0].Records[i] = &kgo.Record{
+				Topic: "test",
+				Value: []byte(fmt.Sprintf("%d", i)),
+			}
+		}
+
+		com := mockCommitter{
+			committedOffsets: []int64{},
+		}
+
+		ctx, cancel := context.WithCancel(t.Context())
+		go func() {
+			time.Sleep(500 * time.Millisecond)
+			cancel()
+		}()
+
+		err := cs.iterationConcurrent(ctx, &com, fetch)
+
+		require.Error(t, err)
+		require.ErrorContains(t, err, "main consumer context error")
+
+		require.LessOrEqual(t, len(processed), 20, "at most 20 events should be processed")
+		require.Len(t, com.committedOffsets, 0, "no events should be committed")
+	})
 }
